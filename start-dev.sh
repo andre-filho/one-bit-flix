63e8eaee52af4a00a4f551ecdf1d8a149a3af240
Andre Filho <andre.filho001@outlook.com>
2018-06-15 10:20:20 -0300

    feature(docker env): creates a docker based environment with postgres database

---
+++ start-dev.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+bundle check || bundle update
+
+while ! pg_isready -h one-bit-flix-database -p 5432 -q -U postgres; do
+	>&2 echo "postgres is unavailable"
+	sleep 1
+done
+
+>&2 echo "postgres is up"
+
+if bundle exec rake db:exists; then
+	bundle exec rails db:create
+	>&2 echo "=========== DATABASE CREATED"
+	bundle exec rails db:migrate
+	>&2 echo "=========== DATABASE MIGRATED"
+else
+	bundle exec rails db:migrate
+	>&2 echo "=========== DATABASE MIGRATED (already exists)"
+fi
+
+bundle exec rails db:seed
+>&2 echo "=========== DATABASE SEEDED"
+
+bundle exec rails s -p 3000 -b '0.0.0.0'
